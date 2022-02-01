7749b2a417f118ec067e03829a2e0018e18b22f3
Serhii Plis <serzh2577@gmail.com>
2022-02-01 16:25:59 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startEl.addEventListener('click', onStartRundomColorForBody);
+stopEl.addEventListener('click', onStopRundomColor);
+
+let timerId = null;
+let isActive = false;
+
+function onStartRundomColorForBody() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  addsRundomColorForBody();
+
+  timerId = setInterval(addsRundomColorForBody, 1000);
+}
+
+function onStopRundomColor() {
+  clearTimeout(timerId);
+  isActive = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function addsRundomColorForBody() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
